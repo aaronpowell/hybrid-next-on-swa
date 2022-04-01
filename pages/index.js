629585c3867209236b30d3a3ae37ad87b060337b
Aaron Powell <me@aaron-powell.com>
2022-04-01 12:01:00 +1100

    adding some SSR stuff

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getServerSideProps() {
+  const data = JSON.stringify({ time: new Date() });
+  return { props: { data } }
+}
+
+export default function Home({ data }) {
+  const serverData = JSON.parse(data);
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +19,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! {serverData.time}</a>
         </h1>
 
         <p className={styles.description}>
